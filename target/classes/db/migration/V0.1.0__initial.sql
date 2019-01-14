0c47866083be38a190886d296609000974984555
Marcelo da Mata Oliveira <mmoliveira@uolinc.com>
2019-01-13 22:50:52 -0200

    Removing target folder. Adding new branch.

--- target/classes/db/migration/V0.1.0__initial.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE hibernate_sequence START WITH 1;
-
-CREATE TABLE customer (
-    id bigint NOT NULL PRIMARY KEY,
-    first_name varchar NOT NULL,
-    last_name varchar NOT NULL,
-    UNIQUE(first_name, last_name)
-);
--- target/classes/db/migration/V0.2.0__sample_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customer (id, first_name,last_name) VALUES (nextval('hibernate_sequence'), 'John', 'Lennon');
-INSERT INTO customer (id, first_name,last_name) VALUES (nextval('hibernate_sequence'), 'Janis', 'Joplin');
