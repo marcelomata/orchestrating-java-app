d71050f745cb159369a361130b65f722f281db48
Marcelo da Mata Oliveira <mmoliveira@uolinc.com>
2019-01-16 01:00:00 -0200

    Getting the db container ip dynamically

--- docker/run.sh
+++ docker/run.sh
@@ -6,3 +6,6 @@ docker network rm migration
 docker network create bravi
 
 ( cd database ; ./run.sh )
+
+db_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bravidb)
+sed -i -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$db_ip/g" ../src/main/resources/application.properties
